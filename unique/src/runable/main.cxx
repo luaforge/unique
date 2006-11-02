d4959dea74b357109ad9bdb6ca4e7ae8803eb5a1
Phoenix RareSky <phoenix11@luaforge.net>
2006-11-02 18:48:25 +0000

    Added test script trunable.lua and smallest changes in make files.

--- unique/src/runable/main.cxx
+++ unique/src/runable/main.cxx
@@ -3,14 +3,23 @@
 #include"modules.hxx"
 #undef UNMODULE
 
+#include"unbase.hxx"
+extern "C" {
+#include"lualib.h"
+}
+
 #define UNMODULE(name) luaopen_ ## name (L);
 int main(int argc,char** argv){
-  lua_State* L=lua_open();
+  lua_State* L=luaL_newstate();
+  luaL_openlibs(L);
+  cout<<"Engine inited...."<<endl;
   #include"modules.hxx"
-  if(argc>2){
+  cout<<"Modules loaded...."<<endl;
+  if(argc>1){
     luaL_loadfile(L,argv[1]);
     lua_call(L,0,2);
   }
   lua_close(L);
+  cout<<"Engine closed...."<<endl;
 }
 #undef UNMODULE
---
+++ unique/trunable.lua
@@ -0,0 +1,60 @@
+#!/usr/bin/env lua5.1
+
+print "Enter lUnique4 session...."
+
+lang="ru_RU"
+print("Change locale to \""..lang.."\" ok!")
+
+function draw()
+   gl.glBegin(gl.GL_TRIANGLES)
+     gl.glVertex3d( 0.0, 1.0, 0.0)
+     gl.glVertex3d(-1.0,-0.5, 0.0)
+     gl.glVertex3d( 1.0,-0.5, 0.0)
+   gl.glEnd()
+end
+
+local cs=unbase.CONTEXTS()
+local c=unogl.GLAPP()
+cs.main=c
+print(cs)
+c.size=unmath.vec2(640,480)
+--c.size=unmath.vec2(1024,768)
+--c.mode=true
+
+c:open()
+
+print(c.state)
+
+local frames=0
+local oldtime,curtime=unbase.TIME(),unbase.TIME()
+local run=true
+
+print(gl.glGetString(gl.GL_VERSION))
+
+while run do
+
+for _,k in c.key do
+  if k.state then
+    if k.code==k.esc then run=false end
+    if k.code==k.f1 then c.mode=not c.mode end
+  end
+end
+
+c:bind()
+
+draw()
+
+c:ubind()
+
+curtime()
+frames=frames+1
+if (curtime-oldtime).time>=1 then
+  oldtime()
+  print(frames)
+  frames=0
+end
+
+end
+
+c:close()
+
